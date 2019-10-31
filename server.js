467351f4c7fd5b87a77a283b058146420d84b6e7
Curtis Charles <ccharles2388@gmail.com>
2019-10-31 13:41:46 -0400

    update server.js with require

--- server.js
+++ server.js
@@ -2,43 +2,65 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const morgan = require('morgan');
+var bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+//==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server 
+// ==============================================================================
 
 const PORT = process.env.PORT || 8081;
 const app = express();
 
 const createApp = () => {
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  process.env.NODE_ENV === 'development' && app.use(morgan('dev'))
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    process.env.NODE_ENV === 'development' && app.use(morgan('dev'))
 
-  app.use('/', require('./server/routes/htmlRoutes'));
-  // app.use('/api', require('./server/routes/apiRoutes'));
+    app.use('/', require('./server/routes/htmlRoutes'));
+    // BodyParser makes it easy for our server to interpret data sent to it.
+    // The code below is pretty standard.
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.text());
+    app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-  // No Need Public Folder In This App 
-  // app.use(express.static(path.join(__dirname,  'public')));
+    // ================================================================================
+    // ROUTER
+    // The below points our server to a series of "route" files.
+    // These routes give our server a "map" of how to respond when users 
+    // visit or request data from various URLs. 
+    // ================================================================================
 
-  app.use((err, req, res, next) => {
-    console.error(err);
-    res.pipe(
-      err.status || 500,
-      err.message || 'Internal Server Error'
-    );
-  });
+    require('./app/routing/api-routes.js')(app);
+    require('./app/routing/html-routes.js')(app);
+
+    // No Need Public Folder In This App 
+    // app.use(express.static(path.join(__dirname,  'public')));
+
+    app.use((err, req, res, next) => {
+        console.error(err);
+        res.pipe(
+            err.status || 500,
+            err.message || 'Internal Server Error'
+        );
+    });
 };
 
 const createConnection = () => {
-  // console.log(db)
+    // console.log(db)
 };
 
 const serverListen = () => {
-  app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+    app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
 };
 
 const run = async () => {
-  await createApp();
-  // Disabled For Now 
-  // await createConnection();
-  await serverListen();
+    await createApp();
+    // Disabled For Now 
+    // await createConnection();
+    await serverListen();
 };
 
 run();
