13346613e7e2a975c2a483c18b53a49c3dc4a924
Curtis Charles <ccharles2388@gmail.com>
2019-10-31 11:44:13 -0400

    friends array

---
+++ app/data/friends.js
@@ -0,0 +1,166 @@
+var friends = [
+    {
+ "name": "Stacy Silas",
+        "photo": "https://scontent-den4-1.xx.fbcdn.net/v/t1.0-1/p320x320/49898055_10216604730648159_4790969442399944704_n.jpg?_nc_cat=109&_nc_ht=scontent-den4-1.xx&oh=b8e0df763b5bc025bb917e45120193b9&oe=5D326B42",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Erin Miller",
+        "photo": "https://scontent-den4-1.xx.fbcdn.net/v/t1.0-1/p320x320/42173485_10101680795411264_5916569526165569536_n.jpg?_nc_cat=108&_nc_ht=scontent-den4-1.xx&oh=0900cea08c218d3c83cd1baf11e4785e&oe=5D3130CF",
+        "scores": [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "Che Previti DeMarco",
+        "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460https://scontent-den4-1.xx.fbcdn.net/v/t1.0-1/p320x320/50955222_1992923034160590_387241673110847488_n.jpg?_nc_cat=109&_nc_ht=scontent-den4-1.xx&oh=92179d9e817269edaa0f50d7a300b507&oe=5D333E73",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Carly Willis",
+        "photo": "https://scontent-den4-1.xx.fbcdn.net/v/t1.0-1/p320x320/48206613_2497291113620965_6847415829722562560_n.jpg?_nc_cat=100&_nc_ht=scontent-den4-1.xx&oh=34b87c8bf53701256910e1c8c27688bc&oe=5D3EDC40",
+        "scores": [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        "name": "Chris Groves",
+        "photo": "https://scontent-den4-1.xx.fbcdn.net/v/t1.0-1/c72.0.320.320a/p320x320/49947887_10217028359477143_8116221444670619648_n.jpg?_nc_cat=109&_nc_ht=scontent-den4-1.xx&oh=b963fb66561f9246784da0101eaa9d5b&oe=5D38C00C",
+        "scores": [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        "name": "Michael Heinemann",
+        "photo": "https://scontent-den4-1.xx.fbcdn.net/v/t1.0-1/p320x320/23844939_10155845237714857_1815845725643545314_n.jpg?_nc_cat=110&_nc_ht=scontent-den4-1.xx&oh=44b233213bf776d49086fa23ab6b6d8b&oe=5D31C18D",
+        "scores": [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    },
+    {
+        "name": "Joel Morgan",
+        "photo": "https://scontent-den4-1.xx.fbcdn.net/v/t1.0-1/p320x320/49946972_10161206148530705_5847387734854860800_n.jpg?_nc_cat=100&_nc_ht=scontent-den4-1.xx&oh=1f75191ee1c2d12dbbd0e6b021add48d&oe=5D2E26E8",
+        "scores": [
+            "1",
+            "1",
+            "1",
+            "2",
+            "3",
+            "2",
+            "2",
+            "2",
+            "5",
+            "1"
+        ]
+    },
+    {
+        "name": "Ryan Proctor",
+        "photo": "https://scontent-den4-1.xx.fbcdn.net/v/t1.0-1/47577377_1994943533894057_2014385355981062144_n.jpg?_nc_cat=102&_nc_ht=scontent-den4-1.xx&oh=93ccb151f8b0b1dffe51b92222ce03a8&oe=5D385960",
+        "scores": [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "3",
+            "3",
+            "3"
+        ]
+    },
+    {
+        "name": "Scott Smith",
+        "photo": "https://scontent-den4-1.xx.fbcdn.net/v/t1.0-1/c59.59.734.734a/s320x320/35651_4133854790739_1670056247_n.jpg?_nc_cat=107&_nc_ht=scontent-den4-1.xx&oh=01fc6d96b82ba1ca722f5ac7502de095&oe=5D057BEF",
+        "scores": [
+            "2",
+            "2",
+            "3",
+            "2",
+            "3",
+            "3",
+            "3",
+            "3",
+            "2",
+            "2"
+        ]
+    },
+    {
+        "name": "Eryn McGarry",
+        "photo": "https://scontent-den4-1.xx.fbcdn.net/v/t1.0-1/c0.0.512.512a/s320x320/48429271_10218854252172440_2167361632819216384_n.jpg?_nc_cat=107&_nc_ht=scontent-den4-1.xx&oh=bc8869fab47d610b8b891d90b2f4b4ad&oe=5D34D7F3",
+        "scores": [
+            "1",
+            "3",
+            "1",
+            "3",
+            "3",
+            "1",
+            "5",
+            "3",
+            "4",
+            "3"
+        ]
+    }
+
+]
+
+// Here we export the array. This makes it accessible to other files using require. 
+module.exports = friends;
